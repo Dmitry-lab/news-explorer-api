9849668659ad781728420ee15a22f9d2ccbf4831
Dmitry Serpkov <dmi-serpkov@yandex.ru>
2020-10-16 00:09:57 +0300

    feat: создать схемы и модели для user и article

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 1000,
+});
+
+mongoose.connect('mongodb://localhost:27017/newsprojectdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.use(limiter);
